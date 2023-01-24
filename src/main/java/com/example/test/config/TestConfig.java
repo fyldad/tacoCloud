758c7d4c48c8dc03102e116489c32a34836fd61e
fyldad <anblazhnov1@gmail.com>
2023-01-24 15:41:12 +0300

    13 - reactive test

--- src/main/java/com/example/test/config/TestConfig.java
+++
@@ -1,9 +0,0 @@
-package com.example.test.config;
-
-import org.springframework.boot.SpringBootConfiguration;
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
-
-@SpringBootConfiguration
-@EnableAutoConfiguration
-public class TestConfig {
-}
